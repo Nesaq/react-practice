35f865a828b23ddb4aa748605c45ff779ecad4cd
NesaQ Nes <kholiknes@gmail.com>
2022-10-11 04:39:23 +0300

    fix date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,13 @@ import './ExpenseItem.css'
 
 
 function ExpenseItem() {
-  // const expenseDate = new Date(2022, 10, 11);
+  const expenseDate = new Date(2022, 10, 11);
   const expenseTitle = 'Car Insurance';
   const expenseAmount = 294.67;
 
   return (
     <div className='expense-item'>
-        <div>{}</div>
+        <div>{expenseDate.toISOString()}</div>
         <div className='expense-item__description'>
           <h2>{expenseTitle}</h2>
           <div className='expense-item__price'>`${expenseAmount}`</div>
