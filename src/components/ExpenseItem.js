16187b7ce5c9eaf41e2667cce7c738621068e8be
Kholik Nesonov <91988209+Nesaq@users.noreply.github.com>
2022-10-11 06:00:49 +0300

    refactor components and added styles

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,16 @@
-import './ExpenseItem.css'
-
+import ExpenseDate from "./ExpenseDate.js";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-
-  const { title, amount, date } = props;
-
   return (
     <div className='expense-item'>
-        <div>{date.toISOString()}</div>
-        <div className='expense-item__description'>
-          <h2>{title}</h2>
-          <div className='expense-item__price'>`${amount}`</div>
-        </div>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ExpenseItem;
